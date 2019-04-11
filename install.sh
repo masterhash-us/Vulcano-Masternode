063330140d067754d06eaabb2d44e6f10836dbf9
Daniel <30388756+whywefight@users.noreply.github.com>
2019-04-11 02:59:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ while [ "$KEY" == "" ]
 do
     KEY=$(whiptail --inputbox "Masternode Privkey" 8 78 --title "Vulcano Masternode Setup" --nocancel 3>&1 1>&2 2>&3)
 done
-echo "masternode=1" >> ~/.VulcanoCore/vulcano.conf
-echo "masternodeprivkey=$KEY" >> ~/.VulcanoCore/vulcano.conf
+echo "masternode=1" >> ~/.vulcanocore/vulcano.conf
+echo "masternodeprivkey=$KEY" >> ~/.vulcanocore/vulcano.conf
 sudo service vulcanod restart
 
 until vulcano-cli getinfo >/dev/null; do
